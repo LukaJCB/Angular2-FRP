66599e93e5a5f4797fe23d9b5e6d7600bdcd5a85
LukaJCB <luka.jacobowitz@fh-duesseldorf.de>
2016-04-03 12:03:13 +0200

    Update for gh-pages.

--- app/server/server.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var app = express();
-app.use("/client", express.static(__dirname + '/../client'));
-app.use("/node_modules", express.static(__dirname + '/../../node_modules'));
-app.use("/", express.static(__dirname + '/../../static'));
-app.listen(3000);
-//# sourceMappingURL=server.js.map
